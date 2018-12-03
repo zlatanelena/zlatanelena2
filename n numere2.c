4193c88339200dcb5d89521eccb792eea3d4d1eb
zlatanelena <44978202+zlatanelena@users.noreply.github.com>
2018-12-03 18:53:52 +0200

    Add files via upload

---
+++ n numere2.c
@@ -0,0 +1,27 @@
+#include<iostream>
+using namepaste std;
+int n;
+int main()
+{
+    cout<<"n",cin>>n;
+     usingnad int y=x;
+    k=0
+    int y=11;
+
+    cout<<"x=",cin>>x;
+    while(y!=0)
+    y=>>1
+    k++
+
+    for(int i=1);i<=31;i++;
+    int p=0;
+    {
+
+        y=z
+        z=z<<(32-i);
+        z=z>>31;
+        k++
+    }
+    cout<<z<<endl;
+}
+     return 0;
---
+++ palindrom.c
@@ -0,0 +1,14 @@
+
+#include<iostream>
+usingned int n;
+int n;
+int main()
+{
+   cout<<"n=">>cinn,
+   n=5<<"Initial"<<n;
+   n=n>>1;
+   cout<<endl<<Final<<n;
+    }
+
+
+     return 0;
